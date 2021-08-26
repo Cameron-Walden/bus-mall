d6c285c3688e5144e71423b88fcf43105f917153
Cameron Walden <cameronkwalden@gmail.com>
2021-08-26 12:52:02 -0700

    got local storage working, working on button

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
 console.log('Take a break. Drink Some water.')
 
 let rounds = 25;
-// const buttonElem = document.getElementById('viewButton')
 const allItemsArray = [];
 
 const ulClickElem = document.getElementById('itemClicks');
@@ -17,6 +16,8 @@ const middlePElem = document.getElementById('middleP');
 
 const rightImgElem = document.getElementById('rightImg');
 const rightPElem = document.getElementById('rightP');
+
+const buttonElem = document.getElementById('viewButton')
 
 let leftItem = null;
 let middleItem = null;
@@ -39,46 +40,57 @@ Item.prototype.renderItem = function (img, p) {
   this.views++;
 }
 
+function storeItems() {
+  const stringifiedItems = JSON.stringify(Item.allItems);
+  // console.log(stringifiedItems)
+  localStorage.setItem('itemsInStorage', stringifiedItems)
+}
+
+// shoutout to david hecker during code review for helping with this
+function getItemsFromStorage() {
+  const stringifiedItems = localStorage.getItem('itemsInStorage')
+  if(stringifiedItems) {
+    const parsedItems = JSON.parse(stringifiedItems)
+    // console.log(parsedItems)
+    for(let item of parsedItems) {
+      const currenItemName = item.name
+      for(let originalItem of Item.allItems) {
+        const originalName = originalItem.name
+        if(currenItemName === originalName) {
+          originalItem.votes = item.votes;
+          originalItem.views = item.views;
+        }
+      }
+    }
+  }  
+}
+
 // function getItemsFromStorage() {
 //   const stringifiedItems = localStorage.getItem('itemsInStorage')
 //   if(stringifiedItems) {
 //     const parsedItems = JSON.parse(stringifiedItems)
-//     console.log(parsedItems)
+//     // console.log(parsedItems)
 //     for(let item of parsedItems) {
 //       const myItem = new Item(item.name, item.image);
-//       Item.allItems.push(myItem);
+//       myItem.votes = item.votes
+//       myItem.Views = item.views
+//       // Item.allItems.push(myItem);
 //     }
-//     renderResults();
+//     // renderResults();
 //   }  else {
 //     // alert('yadayadayada')
-//     // when pushing these items from inside this function, it causes duplicate images to appear, so gotta figure that out.
-//     Item.allItems.push(new Item('R2D2 Rolling Suitcase', './img/bag.jpg'));
-//     Item.allItems.push(new Item('Banana Slicer', './img/banana.jpg'));
-//     Item.allItems.push(new Item('Bathroom Tablet Stand', './img/bathroom.jpg'))
-//     Item.allItems.push(new Item('Open-Toe Rainboots', './img/boots.jpg'))
-//     Item.allItems.push(new Item('3-in-1 Portable Toaster Oven', './img/breakfast.jpg'))
-//     Item.allItems.push(new Item('Meatball Shaped Bubblegum', './img/bubblegum.jpg'))
-//     Item.allItems.push(new Item('Designer Chair', './img/chair.jpg'))
-//     Item.allItems.push(new Item('The Call of Cthulu Action Figure', './img/cthulhu.jpg'))
-//     Item.allItems.push(new Item('Duck Bill Covid Mask for your Dog', './img/dog-duck.jpg'))
-//     Item.allItems.push(new Item('Real Dragon Meat in a Can', './img/dragon.jpg'))
-//     Item.allItems.push(new Item('Cutlery Pen Attachment', './img/pen.jpg'))
-//     Item.allItems.push(new Item('Sweeper-Slippers for your Dog', './img/pet-sweep.jpg'))
-//     Item.allItems.push(new Item('Pizza Scissors and Grafitti Stencil', './img/scissors.jpg'))
-//     Item.allItems.push(new Item('Cozy Shark Sleeping Bag', './img/shark.jpg'))
-//     Item.allItems.push(new Item('Sweeper-Onesie for your Baby', './img/sweep.png'))
-//     Item.allItems.push(new Item('Star wars Tauntaun Sleeping Bag for your Child', './img/tauntaun.jpg'))
-//     Item.allItems.push(new Item('Real Unicorn Meat in a Can', './img/unicorn.jpg'))
-//     Item.allItems.push(new Item('Designer Watering Can', './img/water-can.jpg'))
-//     Item.allItems.push(new Item('Practical Wine Glass', './img/wine-glass.jpg'))
+//     generateItem();
 //   }
-
 // }
 
-// function storeItems() {
-//   const stringifiedItems = JSON.stringify(Item.allItems);
-//   console.log(stringifiedItems)
-//   localStorage.setItem('itemsInStorage', stringifiedItems)
+// function getItemsFromStorage() {
+//   const stringifiedItems = localStorage.getItem('itemsInStorage')
+//   if(stringifiedItems) {
+//     const parsedItems = JSON.parse(stringifiedItems)
+//     for(let item of parsedItems) {
+//       currentName = this.item
+//     }
+//   }
 // }
 
 function getRandomItems() {
@@ -118,10 +130,6 @@ function clickHandle(event) {
       middleItem.votes++;
     } else if (imgClicked === 'rightImg') {
       rightItem.votes++;
-      //trying to get this to work. 
-      // buttonElem.style.display = 'block'
-      // allItemsSectionElem.removeEventListener('click', clickHandle);
-      // renderResults();
     }
   } else {
     alert('Please click on a picture, ' + userName + '.');
@@ -129,13 +137,29 @@ function clickHandle(event) {
   if (rounds === 0) {
     allItemsSectionElem.removeEventListener('click', clickHandle);
     alert('Thank you for voting, ' + userName + '. Check out the chart below.');
-    renderChart();
+    // renderChart();
+    //storeItems();
     // renderResults();
+    storeItems();
+    renderButton();
   } else {
-    renderResults();
+    // renderResults();
     getRandomItems();
-    renderAllItems();
+    // renderAllItems();
   }
+}
+function viewResults() {
+  renderResults();
+  renderChart();
+}
+
+function renderButton() {
+  let buttonElem = document.createElement('button');
+  buttonElem.textContent = 'View Results';
+  buttonElem.id = "view";
+  let buttonDivElem = document.getElementById('viewButton');
+  buttonDivElem.appendChild(buttonElem);
+  buttonElem.addEventListener('click', viewResults)
 }
 
 function renderChart() {
@@ -187,6 +211,15 @@ function renderChart() {
   });
 }
 
+//creating a button to render to the page
+// function renderViewButton() {
+//   let buttonElem = docuemnt.createElement('button')
+//   buttonElem.textContent = 'View Results'
+//   buttonElem.id = 'view'
+//   let buttonDivElem = docuemnt.getElementById('button')
+//   buttonDivElem.appendChild(buttonElem)
+//   buttonElem.addEventListener('click', viewResults)
+// }
 //testing this out still
 // function chartButton() {
 //   renderChart();
@@ -213,6 +246,7 @@ function renderResults() {
 allItemsSectionElem.addEventListener('click', clickHandle);
 // buttonElem.addEventListener('click', chartButton );
 
+
 Item.allItems.push(new Item('R2D2 Rolling Suitcase', './img/bag.jpg'));
 Item.allItems.push(new Item('Banana Slicer', './img/banana.jpg'));
 Item.allItems.push(new Item('Bathroom Tablet Stand', './img/bathroom.jpg'))
@@ -233,6 +267,29 @@ Item.allItems.push(new Item('Real Unicorn Meat in a Can', './img/unicorn.jpg'))
 Item.allItems.push(new Item('Designer Watering Can', './img/water-can.jpg'))
 Item.allItems.push(new Item('Practical Wine Glass', './img/wine-glass.jpg'))
 
-// getItemsFromStorage();
+
+// function generateItems() {
+//   new Item('R2D2 Rolling Suitcase', './img/bag.jpg');
+//   new Item('Banana Slicer', './img/banana.jpg');
+//   new Item('Bathroom Tablet Stand', './img/bathroom.jpg')
+//   new Item('Open-Toe Rainboots', './img/boots.jpg')
+//   new Item('3-in-1 Portable Toaster Oven', './img/breakfast.jpg')
+//   new Item('Meatball Shaped Bubblegum', './img/bubblegum.jpg')
+//   new Item('Designer Chair', './img/chair.jpg')
+//   new Item('The Call of Cthulu Action Figure', './img/cthulhu.jpg')
+//   new Item('Duck Bill Covid Mask for your Dog', './img/dog-duck.jpg')
+//   new Item('Real Dragon Meat in a Can', './img/dragon.jpg')
+//   new Item('Cutlery Pen Attachment', './img/pen.jpg')
+//   new Item('Sweeper-Slippers for your Dog', './img/pet-sweep.jpg')
+//   new Item('Pizza Scissors and Grafitti Stencil', './img/scissors.jpg')
+//   new Item('Cozy Shark Sleeping Bag', './img/shark.jpg')
+//   new Item('Sweeper-Onesie for your Baby', './img/sweep.png')
+//   new Item('Star wars Tauntaun Sleeping Bag for your Child', './img/tauntaun.jpg')
+//   new Item('Real Unicorn Meat in a Can', './img/unicorn.jpg')
+//   new Item('Designer Watering Can', './img/water-can.jpg')
+//   new Item('Practical Wine Glass', './img/wine-glass.jpg')
+//   }
+
 getRandomItems();
-renderAllItems();
+getItemsFromStorage();
+// renderAllItems();
